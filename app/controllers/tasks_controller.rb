06ddbee08e0016e50a7ff206350d8be93a3f198e
Deva Sou <soudeva974@gmail.com>
2020-08-11 16:22:25 +0200

    adding files for tasks manager

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,19 +1,50 @@
 class TasksController < ApplicationController
+before_action :set_task, only: [:show, :edit, :update, :destroy]
   # See all restaurants
   def index
     @tasks = Task.all
   end
 
+  # See details about one restaurant
   def show
-    @task = Task.find(params[:id])
   end
 
+  # Create a restaurant
   def new
     @task = Task.new
   end
 
-  # See details about one restaurant
   # Create a restaurant
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to '/tasks'
+  end
+
   # Update a restaurant
+  def edit
+  end
+
+  def update
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
   # Destroy a restaurant
+  def destroy
+    @task.destroy
+
+    # no need for app/views/restaurants/destroy.html.erb
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
 end
